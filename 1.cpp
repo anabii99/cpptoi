bfd8640a1ef64644ad8803af4886fa05d64b2335
anabii99 <abrorabyyu@duck.com>
2024-03-14 10:51:30 +0700

    add

---
+++ 1.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> unordered_map;
+        for (int i = 0; i < nums.size(); i++) {
+            int value = target - nums[i];
+            if (unordered_map.contains(value)) {
+                return { unordered_map[value], i };
+            }
+            unordered_map[nums[i]] = i;
+        }
+        return {};
+    }
+};
+
+int main() {
+    // Solution sol;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> result = Solution().twoSum(nums, target);
+    cout << "The indices of the two numbers are " << result[0] << " and " << result[1] << "." << endl;
+    return 0;
+}
